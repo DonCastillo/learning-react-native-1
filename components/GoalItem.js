f6bec64ea0580efd41f4056d9c2a51b078f38fd9
Don Castillo <don.qcastillo@yahoo.com>
2023-05-28 22:29:25 -0600

    added hover styles in the pressable button

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,29 +1,34 @@
-import { StyleSheet, View, Text, Pressable } from 'react-native';
+import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem(props) {
-    return (
-        <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-            <View style={styles.goalItem}>
-                <Text style={styles.goalText}>
-                    {props.text}
-                </Text>
-            </View>
-        </Pressable>
-
-    );
+	return (
+		<View style={styles.goalItem}>
+			<Pressable
+				android_ripple={{ color: "#dddddd" }}
+				onPress={props.onDeleteItem.bind(this, props.id)}
+                style={({pressed}) => pressed && styles.pressedItem}
+			>
+				<Text style={styles.goalText}>{props.text}</Text>
+			</Pressable>
+		</View>
+	);
 }
 
 export default GoalItem;
 
 const styles = StyleSheet.create({
-    goalItem: {
+	goalItem: {
 		margin: 8,
-		padding: 8,
 		borderRadius: 6,
 		backgroundColor: "#5e0acc",
 		color: "white",
 	},
 	goalText: {
 		color: "white",
+		padding: 8,
+
 	},
+    pressedItem: {
+        opacity: 0.5
+    }
 });
