60100805147e0e01e7c6571efa907eb156b65441
Don Castillo <don.qcastillo@yahoo.com>
2023-05-29 00:39:02 -0600

    changed bgcolor and status bar color

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Button, StyleSheet, TextInput, View, FlatList } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
@@ -21,7 +22,7 @@ export default function App() {
 			...currentCourseGoals,
 			{ text: enteredGoalText, id: Math.random().toString() },
 		]);
-    endAddGoalHandler();
+		endAddGoalHandler();
 	}
 
 	function deleteGoalHandler(id) {
@@ -32,33 +33,36 @@ export default function App() {
 	}
 
 	return (
-		<View style={styles.appContainer}>
-			<Button
-				title="Add New Goal"
-				color="#5e0acc"
-				onPress={startAddGoalHandler}
-			/>
-			<GoalInput
-				onAddGoal={addGoalHandler}
-				onCancel={endAddGoalHandler}
-				visible={modalIsVisible}
-			/>
-			<View style={styles.goalsContainer}>
-				<FlatList
-					data={courseGoals}
-					renderItem={(itemData) => {
-						return (
-							<GoalItem
-								text={itemData.item.text}
-								id={itemData.item.id}
-								onDeleteItem={deleteGoalHandler}
-							/>
-						);
-					}}
-					alwaysBounceVertical={false}
+		<>
+			<StatusBar style='light'/>
+			<View style={styles.appContainer}>
+				<Button
+					title="Add New Goal"
+					color="#a065ec"
+					onPress={startAddGoalHandler}
 				/>
+				<GoalInput
+					onAddGoal={addGoalHandler}
+					onCancel={endAddGoalHandler}
+					visible={modalIsVisible}
+				/>
+				<View style={styles.goalsContainer}>
+					<FlatList
+						data={courseGoals}
+						renderItem={(itemData) => {
+							return (
+								<GoalItem
+									text={itemData.item.text}
+									id={itemData.item.id}
+									onDeleteItem={deleteGoalHandler}
+								/>
+							);
+						}}
+						alwaysBounceVertical={false}
+					/>
+				</View>
 			</View>
-		</View>
+		</>
 	);
 }
 
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -35,10 +35,18 @@ function GoalInput(props) {
 				/>
 				<View style={styles.buttonContainer}>
 					<View style={styles.button}>
-						<Button title="Add Goal" onPress={addGoalHandler} />
+						<Button
+							title="Add Goal"
+							onPress={addGoalHandler}
+							color="#b180f0"
+						/>
 					</View>
 					<View style={styles.button}>
-						<Button title="Cancel" onPress={props.onCancel} />
+						<Button
+							title="Cancel"
+							onPress={props.onCancel}
+							color="#f31282"
+						/>
 					</View>
 				</View>
 			</View>
@@ -64,9 +72,12 @@ const styles = StyleSheet.create({
 	},
 	textInput: {
 		borderWidth: 1,
-		borderColor: "#cccccc",
+        color: '#120438',
+        borderRadius: 6,
+		borderColor: "#e4d0ff",
+        backgroundColor: "#e4d0ff",
 		width: "100%",
-		padding: 8,
+		padding: 16,
 	},
 	buttonContainer: {
 		flexDirection: "row",
