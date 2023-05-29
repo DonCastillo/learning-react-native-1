1a5d59e5c5eed34bb16c48b43ee834caf0e68b64
Don Castillo <don.qcastillo@yahoo.com>
2023-05-28 23:50:02 -0600

    added cancel button

--- App.js
+++ App.js
@@ -12,11 +12,16 @@ export default function App() {
 		setModalIsVisible(true);
 	}
 
+	function endAddGoalHandler() {
+		setModalIsVisible(false);
+	}
+
 	function addGoalHandler(enteredGoalText) {
 		setCourseGoals((currentCourseGoals) => [
 			...currentCourseGoals,
 			{ text: enteredGoalText, id: Math.random().toString() },
 		]);
+    endAddGoalHandler();
 	}
 
 	function deleteGoalHandler(id) {
@@ -33,7 +38,11 @@ export default function App() {
 				color="#5e0acc"
 				onPress={startAddGoalHandler}
 			/>
-			<GoalInput onAddGoal={addGoalHandler} visible={modalIsVisible}/>
+			<GoalInput
+				onAddGoal={addGoalHandler}
+				onCancel={endAddGoalHandler}
+				visible={modalIsVisible}
+			/>
 			<View style={styles.goalsContainer}>
 				<FlatList
 					data={courseGoals}
